a28ee4b3e8cf828c51310423588502be8c15d752
LjupceSpasovski <ljupce_spasovski@yahoo.com>
2024-04-18 21:10:05 +0200

    Get connection string from parameter store

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const AWS = require('aws-sdk');
 
 const app = express();
 const PORT = 3500;
+
+AWS.config.update({ region: 'us-east-1' });
+
+// Create a new AWS Systems Manager client
+const ssm = new AWS.SSM();
 
 // Middleware
 app.use(bodyParser.json());
@@ -13,13 +19,21 @@ app.use(bodyParser.json());
 //Cors
 app.use(cors());
 
-// MongoDB Connection
-const mongoURI = process.env.MONGO_URI
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// Get connection string from parametar store
+const parametarName = '/myapp/mongodb_uri'
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+async function connectToMongoDB() {
+    try {
+        const mongoURI = await getConnectionString(parametarName);
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+}
+
+// Invoke the function to establish MongoDB connection
+connectToMongoDB();
 
 // MongoDB Model
 const Book = mongoose.model('Book', {
@@ -71,6 +85,14 @@ app.delete('/books/:id', async (req, res) => {
   }
 });
 
+async function getConnectionString(parameterName){
+    try {
+        const response = await ssm.getParameter({Name: parameterName, WithDecryption: true }).promise();
+        return response.Parameter.Value;
+    } catch (error) {
+        console.error("Error ", error)
+    }
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
